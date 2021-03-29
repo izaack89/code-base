fa1f48ea88d98a50b64374042b5add3c76e09918
German Ramirez <german@sidewalk.ai>
2021-03-29 10:12:37 -0600

    Added the function that will store the score and start to create the function to do the quiz

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 <!--
 https://stackoverflow.com/questions/14379274/how-to-iterate-over-a-javascript-object
 
+https://developer.mozilla.org/fr/docs/Web/CSS/Layout_cookbook/List_group_with_badges
+
 https://developer.mozilla.org/es/docs/Web/API/Document/createElement -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,12 @@ var highScoresList = document.querySelector("#highScoresList");
 
 // Section where is defined the variables that I will use on the sysmte
 var secondsLeft = 100;
+var actualScore = 0;
 var historicalScores = [];
+var userData = {
+        inits: "",
+        userScore: ""
+};
 //Section where are the page and the quiz of the system
 var pages = {
     'main': {
@@ -32,7 +37,7 @@ var pages = {
             'properties': {
                 'label': 'Go Back',
                 'type': 'button',
-                'class': 'greenButton',
+                'class': 'purpleButton',
                 'function':'cleanPage();setPage("main")'
             }    
         },
@@ -40,25 +45,143 @@ var pages = {
             'properties': {
                 'label': 'Delete Highscores',
                 'type': 'button',
-                'class': 'greenButton',
-                'function':'setPage("main")'
+                'class': 'purpleButton',
+                'function':'cleanScores()'
             }    
         },
+    },
+    'gameover': {
+        'title': 'All done',
+        'text': 'Your finael score is',
+        'elements':['input1','button1'],
+        'button1': {
+            'properties': {
+                'label': 'Submit',
+                'type': 'button',
+                'class': 'greenButton',
+                'function':'storeScores()'
+            }    
+        },
+        'input1': {
+            'properties': {
+                'label': 'Enter Initials',
+                'id': 'initialUser',
+                'name': 'initialUser',
+                'type': 'input',
+                'class': 'inputClass',
+            }    
+        }
+    },
+    'startquiz': {
+        'title': 'NFL Quiz',
+        'text': '',
+        'elements':[],
     },
 }
 var quiz = [
     {
-        title: "How Many conference are on the NFL:",
-        choices: ["1","2","3", "5"],
-        answer : "2"    
-    }
+        title: "How are the NFL conferences divided?",
+        choice: {
+            a: "AFC, NFC",
+            b: "LFC, NFC",
+            c: "AFC,UFC",
+        },
+        answer : "a"    
+    },
+    {
+        title: "How many teams plays on the NFL:",
+        choice: {
+            a: "52",
+            b: "40",
+            c: "32",
+        },
+        answer : "c"    
+    },
+    {
+        title: "What team never won a Superbowl?",
+        choice: {
+            a: "NE Patriots",
+            b: "KC Chiefs",
+            c: "Dallas Cowboys",
+            d: "Minnesota Vikings",
+        },
+        answer : "d"    
+    },
+    {
+        title: "What are the 4 NFL divisions?",
+        choice: {
+            a: "Up, Down, Left, and Right",
+            b: "East, North, South, and West",
+            c: "AFC 1,AFC 2,NFC 1,NFC 2",
+        },
+        answer : "b"    
+    },
+    {
+        title: "What Super Bowl are we on 2020-2021? ",
+        choice: {
+            a: "LV",
+            b: "LIX",
+            c: "L",
+        },
+        answer : "a"    
+    },
+    {
+        title: "How many Super Bowls are there in total?",
+        choice: {
+            a: "50",
+            b: "44",
+            c: "65",
+            d: "55",
+        },
+        answer : "d"    
+    },
+    {
+        title: "How many Super Bowls are there in total?",
+        choice: {
+            a: "50",
+            b: "44",
+            c: "65",
+            d: "55",
+        },
+        answer : "d"    
+    },
+    {
+        title: "How many Super Bowls are there in total?",
+        choice: {
+            a: "50",
+            b: "44",
+            c: "65",
+            d: "55",
+        },
+        answer : "d"    
+    },
+    {
+        title: "How many Super Bowls are there in total?",
+        choice: {
+            a: "50",
+            b: "44",
+            c: "65",
+            d: "55",
+        },
+        answer : "d"    
+    },
+    {
+        title: "Who wins the Super Bowl 2021?",
+        choice: {
+            a: "Tampa Bay",
+            b: "Kansas City",
+            c: "Dallas",
+            d: "South Carolina",
+        },
+        answer : "a"    
+    },
 ]
 
 
-
 viewScores.addEventListener("click", function () {
-    cleanPage();
+
     setPage('highscores');
+    renderHighScores();
  });
 function setTime() {
 
@@ -78,63 +201,105 @@ function setTime() {
 }
 function cleanPage() {
     titleDiv.textContent = '';
-    sectionElements.textContent = '';    
+    sectionElements.textContent = '';
+    textP.textContent = '';
+    highScoresList.textContent = '';
+    highScoresList.setAttribute('class', '');
+
 }
 function setPage(page) {
-    
+    cleanPage();
     var currentPage = pages[page];
     titleDiv.textContent = currentPage['title'];
     textP.textContent = currentPage['text'];
-            console.log(currentPage['text'])
     for (var [key, elementCreate] of Object.entries(currentPage['elements'])) {
-        console.log(currentPage[elementCreate],elementCreate)
         for (var [key, element] of Object.entries(currentPage[elementCreate])) {
-            console.log(key, element);
+
             var newDiv = document.createElement(element['type']);
-            newDiv.setAttribute('class', element['class']);
-            newDiv.textContent = element['label'];
-            newDiv.setAttribute('onClick', element['function']);
+            if (element['type'] === 'button') {
+                newDiv.setAttribute('class', element['class']);
+                newDiv.textContent = element['label'];
+                newDiv.setAttribute('onClick', element['function']);
+            } else {
+                newDiv.setAttribute('id', element['id']);
+                newDiv.setAttribute('name', element['name']);
+                newDiv.setAttribute('placeholder', element['label']);
+                newDiv.setAttribute('class', element['class']);
+            }
             sectionElements.append(newDiv);
         }
     }
-        
+    if (page==="gameover") {        
+        textP.textContent += " " +actualScore+"."
+    }else if (page==="startquiz") {        
+        startquiz();
+    }
 
 }
 
-setPage('highscores');
+// This function will start the timer and it will display the quiz
 
-renderHighScores();
+function startquiz() {
+    
+}
 
-// The following function renders items in a todo list as <li> elements
+// The following function renders scores on list as <li> elements
 function renderHighScores() {
-  // Clear todoList element and update todoCountSpan
-  todos = JSON.parse(localStorage.getItem("scoresAll"));
-  // Render a new li for each todo
-    console.log(todos)
-  for (var i = 0; i < todos.length; i++) {
-    var initial = todos[i]["inits"];
-    var score  = todos[i]["userScore"];
-    var li = document.createElement("li");
-    li.textContent = initial + "   --  "+score;
-    li.setAttribute("data-index", i);
-    highScoresList.appendChild(li);
-  }
+  // Get the Scores from the local storage
+    var rankNumber = 1;
+    // Adding the class list-group
+    highScoresList.setAttribute('class', 'list-group');
+  // Render a new li for each scores that is inside of local storage
+    console.log(historicalScores.length);
+    if (historicalScores.length > 0) {
+        for (var i = 0; i < historicalScores.length; i++) {
+            var initial = historicalScores[i]["inits"];
+            var score = historicalScores[i]["userScore"];
+            var li = document.createElement("li");
+            li.textContent = rankNumber + ".  " + initial;
+            li.setAttribute("data-index", i);
+            var span = document.createElement("span");
+            span.setAttribute('class', 'badge');
+            span.textContent = score;
+            li.append(span);
+            highScoresList.append(li);
+            rankNumber++;
+        }
+    } else {
+        
+            var li = document.createElement("li");
+            li.textContent = " No Scores ";
+            highScoresList.append(li);
+    }
 }
+// Function that save the score of the person that resolve the quiz
+function storeScores() {
 
-function storeTodos() {
-  // Stringify and set key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// DOing test on the localstorage
-    var userData = {
-        inits: "GR",
-        userScore: "8"
-    };
-    historicalScores.push(userData);
-var userData = {
-        inits: "IR",
-        userScore: "9"
-    };
+    userData.inits = document.getElementById('initialUser').value;
+    userData.userScore = actualScore;
     historicalScores.push(userData);
     localStorage.setItem("scoresAll", JSON.stringify(historicalScores));
+    highScoresList.textContent = '';
+    setPage('highscores');
+    renderHighScores();
+}
+// This function helps to clear the Highscores
+function cleanScores() {
+    historicalScores = [];
+    localStorage.setItem("scoresAll", '');
+    highScoresList.textContent = '';
+    renderHighScores();
+}
+
+    
+
+
+// Init the Page to main
+function init() {
+    
+    localStorage.setItem("scoresAll", JSON.stringify(historicalScores));
+    historicalScores=  JSON.parse(localStorage.getItem("scoresAll"));
+    setPage('startquiz');
+}
+
+init();
